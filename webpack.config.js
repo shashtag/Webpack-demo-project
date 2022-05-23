61be3a565b84dc2e3db98d370c94eda224707b32
shahtag <shashwatsatna@gmail.com>
2022-05-23 17:06:31 +0530

    ✨ add loaders

--- webpack.config.js
+++ webpack.config.js
@@ -4,4 +4,9 @@ module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: { filename: "main.js", path: path.resolve(__dirname, "dist") },
+  module: {
+    rules: [
+      { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
+    ],
+  },
 };
