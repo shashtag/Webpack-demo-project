482dddb4abbbce679226a1ae7f0f7035b83208c9
shahtag <shashwatsatna@gmail.com>
2022-05-23 18:09:29 +0530

    ✨ loading files

--- webpack.common.js
+++ webpack.common.js
@@ -7,6 +7,14 @@ module.exports = {
   module: {
     rules: [
       { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
+      { test: /\.html$/, use: ["html-loader"] },
+      {
+        test: /\.(svg|png|jpeg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: { name: "[name].[hash].[ext]", outputPath: "imgs" },
+        },
+      },
     ],
   },
 };
