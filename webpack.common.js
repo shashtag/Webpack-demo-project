22bfa4743747d4b948fe38cda7dce24033bacfb6
shahtag <shashwatsatna@gmail.com>
2022-05-23 17:47:13 +0530

    ✨ dev server and hot reloading

--- webpack.common.js
+++ webpack.common.js
@@ -2,12 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
-  output: {
-    filename: "main.[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
-  },
   plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
   module: {
     rules: [
