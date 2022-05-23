7f2ad30da37f835b5c221c62bc16591cfe0b6cc2
shahtag <shashwatsatna@gmail.com>
2022-05-23 19:03:24 +0530

    ⚡️ optimizations

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,9 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: { main: "./src/index.js", vendor: "./src/vendor.js" },
-  plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })],
   module: {
     rules: [
-      { test: /\.scss$/, use: ["style-loader", "css-loader", "sass-loader"] },
       { test: /\.html$/, use: ["html-loader"] },
       {
         test: /\.(svg|png|jpeg|gif)$/,
--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,10 @@ const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
 
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("Terser-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -10,5 +14,32 @@ module.exports = merge(common, {
     filename: "[name].[contenthash].bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+      minify: {
+        removeAttributeQuotes: true,
+        collapseWhitespace: true,
+        removeComments: true,
+      },
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+      // `...`,
+      new CssMinimizerPlugin(),
+      new TerserPlugin(),
+    ],
+  },
 });
